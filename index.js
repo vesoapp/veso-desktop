4b8398b33e8a6c09714576a099c815b5acfd0ea1
Danny Michel <dan@varmag.com>
2022-11-03 03:08:24 -0400

    Veso

---
+++ index.js
@@ -0,0 +1,16 @@
+const {BrowserWindow, app} = require('electron');
+require('v8-compile-cache');
+
+app.on('ready', () => {
+    const win = new BrowserWindow({
+        frame: false,
+        width: 1280,
+        height: 720,
+        title: 'Veso'
+    });
+    win.loadURL(`file://${__dirname}/dist/index.html`);
+});
+
+app.on('window-all-closed', () => {
+    app.quit();
+});
